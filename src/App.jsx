7c70d7dd1f4ac406c94ab44ebd8ddfaacbab0054
Sharon Lee <ssmlee10@gmail.com>
2025-08-09 09:15:29 -0700

    create bookshelf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import './App.css';
+import Bookshelf from './components/Bookshelf/Bookshelf.jsx';
+
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
+    </>
+  );
 };
 
 export default App;
